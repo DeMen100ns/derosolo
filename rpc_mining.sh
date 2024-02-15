6016a01c2c8f612d8e74c8104d6d0a6b59942e17
Vo Khac Trieu <72329092+DeMen100ns@users.noreply.github.com>
2024-02-15 13:23:20 +0700

    Update rpc_mining.sh

--- rpc_mining.sh
+++ rpc_mining.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "Replace dero1qyjx7f99y5u3a2py775nzp0qg9j0shyf509jkpukly8s87lv73crvqgrgy3rf, dero.rabidmining.com:10300 to run the miner"
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
 while :; do
-    ./astrominer -w dero1qyjx7f99y5u3a2py775nzp0qg9j0shyf509jkpukly8s87lv73crvqgrgy3rf -r dero.rabidmining.com:10300 -p rpc;
+    ./astrominer -w dero1qyjx7f99y5u3a2py775nzp0qg9j0shyf509jkpukly8s87lv73crvqgrgy3rf -r 192.168.1.2:10100 -r1 dero.rabidmining.com:10300 -p rpc;
     sleep 5;
 done
